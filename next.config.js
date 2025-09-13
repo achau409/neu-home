08cd56cac0d5ba98e27daee9d754042f63566d11
Omor Faruk Rony <omorfarukrony2@gmail.com>
2025-09-13 23:26:39 +0600

    Update next.config.js to format API rewrites consistently and add a new rewrite rule for goflooroffers.com domain to redirect to /go-flooring.

--- next.config.js
+++ next.config.js
@@ -11,16 +11,30 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'https://neo-cms.vercel.app/api/:path*',
+        source: "/api/:path*",
+        destination: "https://neo-cms.vercel.app/api/:path*",
       },
       {
-        source: '/ingest/static/:path*',
-        destination: 'https://us-assets.i.posthog.com/static/:path*',
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
       },
       {
-        source: '/ingest/:path*',
-        destination: 'https://us.i.posthog.com/:path*',
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+      {
+        source: "/",
+        has: [
+          {
+            type: "host",
+            value: "goflooroffers.com", // apply ONLY for this domain
+          },
+          {
+            type: "host",
+            value: "www.goflooroffers.com", // include www if used
+          },
+        ],
+        destination: "/go-flooring",
       },
     ];
   },
